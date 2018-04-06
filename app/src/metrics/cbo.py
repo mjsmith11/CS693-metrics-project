ad67b7de0bb5ea6203a8e1eef120ea1444779031
Matt Smith <mjsmith92@outlook.com>
2018-04-05 22:48:34 -0400

    Add class to find "interesting" variables for CBO

--- app/src/metrics/cbo.py
+++ app/src/metrics/cbo.py
@@ -12,21 +12,43 @@ class CBO:
     def findClasses(self):
         classVisiter = ClassNodeLister()
         classVisiter.visit(self.tree)
+
         self.classNodes = classVisiter.getClassNodes()
 
     def playground(self):
         self.findClasses()
-        couplingClass = self.classNodes[2]
-        AttributeLister().visit(couplingClass)
+        for cl in self.classNodes:
+            print(cl.name)
+            var = ListVariablesOfType("CouplingExample1",False)
+            var.visit(cl)
+            print(var.getVariablesList())
+            #var = ClassVariableLister()
+            #var.visit(cl)
+            #print(var.getVariablesList()
 
-class AttributeLister(ast.NodeVisitor):
-    def __init__(self):
-        self.attributesReferenced = []
+# List all variables of `searchType` as determined by constructor calls in the node searched.
+# If `attributeVars` is True, a list of class attributes will be built.
+# If `attributeVars` is False, a list of local variables will be built.
+class ListVariablesOfType(ast.NodeVisitor):
+    def __init__(self, searchType, attributeVars):
+        self.variables = []
+        self.type = searchType
+        self.attributeVars = attributeVars
 
-    def getAttributeList(self):
-        return self.attributesReferenced
+    def getVariablesList(self):
+        return self.variables
 
-    def visit_Attribute(self, node):
-        self.attributesReferenced.append(node.attr)
-        print(ast.dump(node))
-        self.generic_visit(node)
+    def visit_Assign(self, node):
+        value = getattr(node, 'value', None)
+        if isinstance(value, ast.Call): # Is it making a call on the RHS
+            func = getattr(value, 'func', None)
+            if isinstance(func,ast.Name) and func.id==self.type: # Does RHS fun have the name we are looking for?
+                for target in node.targets:
+                    if self.attributeVars:
+                        if isinstance(target,ast.Attribute) and target.value.id == "self": #is the LHS an attribute belonging to self?
+                            if target.attr not in self.variables:
+                                self.variables.append(target.attr)
+                    else:
+                        if isinstance(target,ast.Name): # is the target a name?
+                            if target.id not in self.variables:
+                                self.variables.append(target.id)
